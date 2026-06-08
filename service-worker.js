cefc00d592ed67ef15b5c66890baf514f1da08bb
hearthealthhub <bamzprojects16@gmail.com>
2026-06-08 15:39:23 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const cacheName = "hhh-finance-v4";
+const cacheName = "hhh-finance-v5";
 const appShell = [
   "/",
   "/index.html",
-  "/styles.css?v=4",
-  "/app.js?v=4",
+  "/styles.css?v=5",
+  "/app.js?v=5",
   "/manifest.json",
   "/icon.svg"
 ];
