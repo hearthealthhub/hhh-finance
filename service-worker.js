441989c6906128a739ef9dfcf87349899997c93c
hearthealthhub <bamzprojects16@gmail.com>
2026-06-10 14:04:18 +0100

    Improve invoices and product management

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const cacheName = "hhh-finance-v10";
+const cacheName = "hhh-finance-v11";
 const appShell = [
   "./",
   "index.html",
-  "styles.css?v=10",
-  "app.js?v=10",
+  "styles.css?v=11",
+  "app.js?v=11",
   "manifest.json",
   "icon.svg"
 ];
